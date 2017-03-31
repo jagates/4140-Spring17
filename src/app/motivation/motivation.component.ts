13784e4bb4cc1371e0380d89c3063854ff58c8a5
Bryan Pyle <bpyle0092@gmail.com>
2017-03-31 11:28:40 -0400

    Motivation: tabs working for progress card

--- src/app/motivation/motivation.component.ts
+++ src/app/motivation/motivation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MdDialog, MdDialogRef} from '@angular/material';
 
 @Component({
   selector: 'app-motivation',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./motivation.component.scss']
 })
 export class MotivationComponent implements OnInit {
-
+  newGoal: string = '';
   tiles = [
     {text: 'Daily Goals', cols: 2, rows: 3, color: 'lightblue'},
     {text: 'Progress', cols: 2, rows: 3, color: 'lightgreen'},
@@ -22,22 +23,39 @@ export class MotivationComponent implements OnInit {
   ];
 
   interests = [
-    {title: '10 Quick Paleo Breakfasts', avatar: 'breakfast.jpg', source: 'eathealthy.com', upvotes: '1.5k', description: 'New recipes from Jillian for on-the-go Paleo dieters ...'},
-    {title: 'What Time of Day Should You Run?', avatar: 'run.jpg', source: 'runninglife.com', upvotes: '1.2k', description: 'New studies show you how to get the most out of your run ...'},
-    {title: 'How Paleo Improves Your Liver', avatar: 'liver.jpg', source: 'paleofoodies.com', upvotes: '1.2k', description: 'Dr. Richard Feinbaum explains how your liver is dependent on protein ...'},
-    {title: 'That Sugar Film!', avatar: 'sugarfilm.jpg', source: 'netflix.com', upvotes: '1.1k', description: 'Damon Gameau embarks on a unique experiment to document the effects of a high ...'},
-    {title: 'Cardio Before or After Weightlifting?', avatar: 'cardio.jpg', source: 'buildingbody.com', upvotes: '951', description: 'Our experts explain the primetime to run for mass ...'}
+    {title: '10 Quick Paleo Breakfasts', avatar: 'breakfast.jpg', source: 'eathealthy.com', upvotes: '1.5k', description: 'New recipes from Jillian for on-the-go Paleo dieters ...', liked: false},
+    {title: 'What Time of Day Should You Run?', avatar: 'run.jpg', source: 'runninglife.com', upvotes: '1.2k', description: 'New studies show you how to get the most out of your run ...', liked: false},
+    {title: 'How Paleo Improves Your Liver', avatar: 'liver.jpg', source: 'paleofoodies.com', upvotes: '1.2k', description: 'Dr. Richard Feinbaum explains how your liver is dependent on protein ...', liked: false},
+    {title: 'That Sugar Film!', avatar: 'sugarfilm.jpg', source: 'netflix.com', upvotes: '1.1k', description: 'Damon Gameau embarks on a unique experiment to document the effects of a high ...', liked: false},
+    {title: 'Cardio Before or After Weightlifting?', avatar: 'cardio.jpg', source: 'buildingbody.com', upvotes: '1.0k', description: 'Our experts explain the primetime to run for mass ...', liked: false}
 
   ];
 
-  goals = [
+  goals:string[] = [
     'Go for a run',
     'Eat less than 2300 calories',
     'Drink 3L of water'
-  ]
+  ];
+
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  addNewGoal():void {
+    if(this.newGoal.length > 0) {
+      this.goals.push(this.newGoal);
+      this.newGoal = '';
+    }
+  }
+  removeGoal(index: number):void {
+    console.log("Goal: " + this.goals[index]);
+    this.goals.splice(index,1);
+    console.log(this.goals);
+  }
+
+  toggleLike(index: number): void {
+    this.interests[index].liked = !this.interests[index].liked;
+  }
 }
