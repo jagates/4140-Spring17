71b7c513561fbb82084083ce371f3f3ba4655b83
Bryan Pyle <bpyle0092@gmail.com>
2017-04-01 19:20:03 -0400

    Motivation: progress works

--- src/app/motivation/motivation.component.ts
+++ src/app/motivation/motivation.component.ts
@@ -32,9 +32,19 @@ export class MotivationComponent implements OnInit {
   ];
 
   goals:string[] = [
+    'Enter body weight',
     'Go for a run',
     'Eat less than 2300 calories',
-    'Drink 3L of water'
+  ];
+
+  progressions = [
+    {label: 'Week', wgraph: 'weekweight.png', weight: +0.8, wtarget: -0.7, rgraph: 'weekrun.png', rtotal: 10.4, ravg: 3.03, rlast: +1.1, rtarget: 10, dtotal: 15200, dtarget: 16100},
+    {label: 'Month', wgraph: 'monthweight.png', weight: -3.2, wtarget: -2.8, rgraph: 'monthrun.png', rtotal: 28.3, ravg: 2.44, rlast: +5.2, rtarget: 40, dtotal: 63000, dtarget: 60800},
+    {label: 'Year', wgraph: 'yearweight.png', weight: -18.1, wtarget: -20.6, rgraph: 'yearrun.png', rtotal: 9.1, ravg: 3.03, rlast: +1.1, rtarget: 480},
+  ];
+
+  graphbools: boolean[] = [
+    false, false, false
   ];
 
 
@@ -57,5 +67,9 @@ export class MotivationComponent implements OnInit {
 
   toggleLike(index: number): void {
     this.interests[index].liked = !this.interests[index].liked;
+  }
+
+  toggleGraph(index:number): void {
+    this.graphbools[index] = !this.graphbools[index];
   }
 }
