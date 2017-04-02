09fd37c5a7142f23b8c6ec22e6aa860c10973475
Tyler McCall <tyler@flaremaster.net>
2017-04-02 17:02:20 -0400

    send interests paged for review

--- src/app/interests/interests.component.ts
+++ src/app/interests/interests.component.ts
@@ -21,11 +21,17 @@ export class InterestsComponent implements OnInit {
   ];
   pdNews = [
     {title: '10 Quick Paleo Breakfasts', avatar: 'breakfast.jpg', source: 'eathealthy.com', upvotes: '1.5k', description: 'New recipes from Jillian for on-the-go Paleo dieters ...', liked: false},
-    {title: 'How Paleo Improves Your Liver', avatar: 'liver.jpg', source: 'paleofoodies.com', upvotes: '1.2k', description: 'Dr. Richard Feinbaum explains how your liver is dependent on protein ...', liked: false}
+    {title: 'How Paleo Improves Your Liver', avatar: 'liver.jpg', source: 'paleofoodies.com', upvotes: '1.2k', description: 'Dr. Richard Feinbaum explains how your liver is dependent on protein ...', liked: false},
+    {title: 'B.Traits on how Paleo diet keeps her fit', avatar: 'pdk.jpg', source: 'mensjournal.com', upvotes: '1.1k', description: 'It makes me feel stronger as a human being', link: 'http://mixmag.net/feature/b-traits-health-season/', liked: false},
+    {title: 'Is the Paleo Diet for You?', avatar: 'pdm.jpg', source: 'mixmag.net', upvotes: '1.0k', description: 'Paleo can be a good way to increase energy, improve gut health, and even lose a few pounds.', link: 'http://www.mensjournal.com/health-fitness/articles/is-the-paleo-diet-for-you-w465200', liked: false},
+    {title: 'What is the Paleo diet?', avatar: 'pde.png', source: 'metro.co.uk', upvotes: '9k', description: 'The Paleo diet is based on the premise that we should take our diet back to its simplest form – the diet followed by our earliest ancestors.', link: 'http://metro.co.uk/2017/01/02/what-is-the-paleo-diet-and-what-can-you-eat-6356607/http://metro.co.uk/2017/01/02/what-is-the-paleo-diet-and-what-can-you-eat-6356607/', liked: false}
   ];
   caNews = [
     {title: 'What Time of Day Should You Run?', avatar: 'run.jpg', source: 'runninglife.com', upvotes: '1.2k', description: 'New studies show you how to get the most out of your run ...', liked: false},
-    {title: 'Cardio Before or After Weightlifting?', avatar: 'cardio.jpg', source: 'buildingbody.com', upvotes: '1.0k', description: 'Our experts explain the primetime to run for mass ...', liked: false}
+    {title: 'Cardio Before or After Weightlifting?', avatar: 'cardio.jpg', source: 'buildingbody.com', upvotes: '1.1k', description: 'Our experts explain the primetime to run for mass ...', liked: false},
+    {title: '4 Must-Try Cardio Workouts', avatar: 'cardio.jpg', source: 'webmd.com', upvotes: '1.0k', description: 'You\'re at the gym, ready to do your cardio. Today, don\'t do the exact same thing you always do.', liked: false},
+    {title: 'Cardio Workouts: Fix Yours', avatar: 'cwm.jpg', source: 'womenshealth.com', upvotes: '9k', description: 'Think you\'re doing the best cardio workouts and exercises?', liked: false},
+    {title: '11 ways to build your cardio workout', avatar: 'cml.jpg', source: 'mensfitness.com', upvotes: '8k', description: 'Stop slogging away in treadmill purgatory.', liked: false}
 
   ];
 
