96280e548caa1263305062978be16e90be08a841
SaiC <SaiC@213.202.21.198.tigernet.wifi.dyn.clemson.edu>
2017-04-02 17:16:36 -0400

    *updated groups and goals

--- src/app/goals/goals.component.ts
+++ src/app/goals/goals.component.ts
@@ -7,9 +7,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GoalsComponent implements OnInit {
 
+  newGoal: string = '';
+  newGoal2: string = '';
+
+
+  tiles = [
+    {text: 'Daily Goals', cols: 4, rows: 3, color: 'lightblue'},
+    {text: 'Progress', cols: 4, rows: 3, color: 'lightblue'},
+    //{text: '', cols: 4, rows: 3, color: 'lightblue'},
+
+  ];
+
+  goals:string[] = [
+    'Enter body weight',
+    'Go for a run',
+    'Eat less than 2300 calories',
+  ];
+
+  goals2:string[] = [
+    'Quit Smoking',
+    'Get a six pack',
+    'Bench 215 LBS',
+  ];
+
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addNewGoal():void {
+    if(this.newGoal.length > 0) {
+      this.goals.push(this.newGoal);
+      this.newGoal = '';
+    }
+  }
+  removeGoal(index: number):void {
+    console.log("Goal: " + this.goals[index]);
+    this.goals.splice(index,1);
+    console.log(this.goals);
+  }
+  addNewGoal2():void {
+    if(this.newGoal2.length > 0) {
+      this.goals2.push(this.newGoal2);
+      this.newGoal2 = '';
+    }
+  }
+  removeGoal2(index: number):void {
+    console.log("Goal: " + this.goals2[index]);
+    this.goals2.splice(index,1);
+    console.log(this.goals2);
   }
 
 }
